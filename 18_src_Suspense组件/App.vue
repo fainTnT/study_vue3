69f31305d35496b7f7b758ddbfd81aec942f059f
fainTnT <614604214@qq.com>
2021-09-01 12:08:26 +0800

    vue3_basic

---
+++ 18_src_Suspense组件/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class='app'>
+    <h1>父组件</h1>
+    <!-- 2.使用Suspense(悬念的)包裹组件,default是需要展示的组件,fallback是组件没展示出来所显示的组件 -->
+    <Suspense>
+      <template v-slot:default>
+        <Father></Father> 
+      </template>
+      <template v-slot:fallback>
+        <h1>loading...</h1>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script>
+// 静态引入
+// import Father from './Father.vue'
+
+// 1.异步引入
+import {defineAsyncComponent} from 'vue'
+const Father = defineAsyncComponent(()=>import('./Father.vue'))
+export default {
+  name: "App",
+  components:{
+    Father
+  },
+  setup() {
+
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+.app {
+  width:100%;
+  padding:10px;
+  background-color: skyblue;
+}
+</style>
---
+++ 18_src_Suspense组件/Father.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="father">
+    <h1>子组件:{{sum}}</h1>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  setup(){
+    let sum = ref(0)
+    return new Promise((resolve,reject) => {
+      setTimeout(()=>{
+        resolve({
+          sum
+        })
+      },2000)
+    })
+  }
+}
+</script>
+
+<style>
+.father {
+  width:100%;
+  background-color: pink;
+  padding:10px;
+}
+</style>
